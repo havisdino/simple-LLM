a19a1ddd444be2eea39aa7d548effc738f820c2e
havisdino <hadinh893@gmail.com>
2024-04-15 19:42:23 +0700

    Update default tokenizer path

--- inference.py
+++ inference.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from tokenizers import Tokenizer
 import torch
 
-from config import DEVICE
+from config import DEVICE, TOKENIZER_PATH
 from sampler import Sampler
 from modules import get_model_from_config
 
@@ -13,18 +13,16 @@ if __name__ == '__main__':
 
     parser.add_argument('--checkpoint', type=str, required=True)
     parser.add_argument('--device', type=str, default=DEVICE)
-    parser.add_argument('--tokenizer', type=str, default='tokenizer/byte-level-bpe-tinystories.json')
+    parser.add_argument('--tokenizer', type=str, default=TOKENIZER_PATH)
     parser.add_argument('--robust', type=float, default=3.)
     parser.add_argument('--topk', type=int, default=5)
     parser.add_argument('--seed', type=str, required=True)
-
 
     args = parser.parse_args()
 
     checkpoint = torch.load(args.checkpoint)
     model = get_model_from_config()
     model.load_state_dict(checkpoint['model'])
-    
     
     tokenizer = Tokenizer.from_file(args.tokenizer)
 
