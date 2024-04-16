12770c5ebd20f3b24c638150f847184cad28008d
havisdino <hadinh893@gmail.com>
2024-04-16 21:43:52 +0700

    Update tokenizer trainer

--- tokenizer/train_tokenizer.py
+++ tokenizer/train_tokenizer.py
@@ -1,20 +1,16 @@
-import sys
-sys.path.append('..')
-
 from argparse import ArgumentParser
 from tokenizers import ByteLevelBPETokenizer
-
-from config import *
 
 
 parser = ArgumentParser()
 parser.add_argument('-f', '--files', nargs='+', required=True)
+parser.add_argument('-s', '--vocab-size', type=int, required=True)
 parser.add_argument('-d', '--destination', type=str, required=True)
 
 args = parser.parse_args()
 
 tokenizer = ByteLevelBPETokenizer()
-tokenizer.train(args.files, vocab_size=VOCAB_SIZE - 2)
+tokenizer.train(args.files, vocab_size=args.vocab_size - 2)
 tokenizer.add_special_tokens(['<end>', '<sum>'])
 
 tokenizer.save(args.destination)
